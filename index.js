72f4054f96fcd8aa89c5a1adcb02648b31f560a4
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-11-02 00:17:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  // 1. figure out how long our password should break
+  
+  // 2. We need to make a password from the ground up 
+  
+  // 3. We need to keep track of requirements
+  
 });
